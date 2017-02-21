f2e2b02ee69ee5ea1a72990966f6c44c69d72e31
Théo Andrieux <tandrieux@icloud.com>
2017-02-21 18:22:52 +0100

    [ADD] Ajout module correction règles de réappro
    Correction de la date de commande et de la date de réception prévue lors de la génération d'une demande de prix à partir des règles de réapprovisionnement

---
+++ stock_fix/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Stock - Fix",
+
+    'summary': """Fix bugs in Stock module""",
+
+    'description': """
+    """,
+
+    'author': "e-COSI",
+    'website': "http://www.e-cosi.com",
+    'category': 'Stock',
+    'version': '0.1',
+    'depends': ['base', 'stock'],
+}
